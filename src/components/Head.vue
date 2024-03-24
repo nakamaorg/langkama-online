853c3608fb43355db580cf7677cecf58ea4e2ff5
eoussama <oussama.essamadi@gmail.com>
2024-03-24 14:41:28 +0000

    head

--- src/components/Head.vue
+++ src/components/Head.vue
@@ -1,7 +1,93 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+import Tag from 'primevue/tag';
+import Image from 'primevue/image';
+import Button from 'primevue/button';
+import Menubar from 'primevue/menubar';
+import Tooltip from 'primevue/tooltip';
+
+const config = reactive({ ...__CONFIG__ });
+
+function onSourceCode(e: MouseEvent): void {
+  e.stopPropagation();
+  window.open('https://github.com/nakamaorg/langkama-online', '_blank');
+}
+
+function clear(e: MouseEvent): void {
+  e.stopPropagation();
+}
+
+function run(e: MouseEvent): void {
+  e.stopPropagation();
+}
+</script>
+
 <template>
   <div class="root">
-    <nav>
+    <Menubar class="nav">
+      <template #start>
+        <div class="nav__start">
+          <img class="logo" src="/imgs/logo/logo.png" alt="Nakama Realm Logo">
 
-    </nav>
+          <h1 class="title">
+            LangKama Online
+
+            <span class="version">
+              <Tag severity="secondary" :value="config.version"></Tag>
+            </span>
+          </h1>
+        </div>
+      </template>
+
+      <template #end>
+        <div class="nav__end">
+          <Button v-tooltip.bottom="'Source Code'" icon="pi pi-github" severity="secondary" aria-label="New"
+            @click="onSourceCode" />
+          <Button v-tooltip.bottom="'Clear'" icon="pi pi-delete-left" severity="secondary" aria-label="New"
+            @click="clear" />
+          <Button v-tooltip.bottom="'Run'" icon="pi pi-play" aria-label="Interpret" @click="run" />
+        </div>
+      </template>
+    </Menubar>
   </div>
 </template>
+
+<style scoped lang="scss">
+.root {
+  .nav {
+    display: flex;
+    flex-direction: row;
+
+    align-items: center;
+    justify-content: space-between;
+
+    &__start,
+    &__end {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+
+    &__start {
+      .logo {
+        width: 40px;
+        margin-right: 8px;
+        border-radius: 5px;
+      }
+
+      .title {
+        font-size: 18px;
+        color: var(--color-primary);
+        font-weight: var(--font-weight-bold);
+      }
+    }
+
+    &__end {
+      >* {
+        margin-left: 10px;
+      }
+    }
+  }
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import './theme/main.scss';
+import 'primeicons/primeicons.css';
+import 'primevue/resources/themes/aura-light-green/theme.css';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import Tooltip from 'primevue/tooltip';
+import PrimeVue from 'primevue/config';
 import { install as VueMonacoEditorPlugin } from '@guolao/vue-monaco-editor'
 
 import App from './App.vue';
 
 createApp(App)
+  .use(PrimeVue)
   .use(createPinia())
   .use(VueMonacoEditorPlugin, { paths: { vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.43.0/min/vs' } })
+  .directive('tooltip', Tooltip)
   .mount('#app');

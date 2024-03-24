ff09740f091c6f0f00f0aaa872ba9f6a22f7aa40
eoussama <oussama.essamadi@gmail.com>
2024-03-24 13:47:25 +0000

    page composition

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,14 @@
 <script setup lang="ts">
-import { reactive } from 'vue';
-
-const config = reactive({
-  ...__CONFIG__
-});
-
+import Home from './components/Home.vue';
 </script>
 
 <template>
   <div class="root">
-    <header>
-      <img src="/imgs/logo/logo.png" alt="Nakama Realm Logo">
-
-      <h1>LangKama Online {{ config.version }}</h1>
-    </header>
-
-    <main>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita vel maxime repudiandae vitae quae. Pariatur unde
-      minus recusandae ducimus beatae aperiam esse deserunt tempore rem, omnis aut saepe! Dolore, eum.
-    </main>
+    <Home />
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .root {
   width: 100%;
   height: 100%;
---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Head from './Head.vue';
+import Editor from './Editor.vue';
+import Console from './Console.vue';
+</script>
+
+<template>
+  <div class="root">
+    <div class="head">
+      <Head />
+    </div>
+    <div class="body">
+      <div class="editor">
+        <Editor />
+      </div>
+      <div class="console">
+        <Console />
+      </div>
+    </div>
+  </div>
+</template>
